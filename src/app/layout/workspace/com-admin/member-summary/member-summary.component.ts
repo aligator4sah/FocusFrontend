6fb0815868b1d555588f247b31dd0a0498a408b7
Aligator_sah <saixiongh@outlook.com>
2018-06-02 22:48:46 -0400

    add graph to member summary

--- src/app/layout/workspace/com-admin/member-summary/member-summary.component.ts
+++ src/app/layout/workspace/com-admin/member-summary/member-summary.component.ts
@@ -7,6 +7,32 @@ import {SummaryService} from "../../../../service/summary.service";
   styleUrls: ['./member-summary.component.css']
 })
 export class MemberSummaryComponent implements OnInit {
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 
   adminLoc = JSON.parse(localStorage.getItem('curUser')).location;
 
@@ -31,5 +57,14 @@ export class MemberSummaryComponent implements OnInit {
   }
 
 
+  selected: string;
+
+  features = [
+    'Gender',
+    'Race',
+    'Marry',
+    'Education',
+    'Employments',
+  ];
 
 }
