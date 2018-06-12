38c7a16652b2b666296a9842fbcfd0bdbb3909b2
Aligator_sah <saixiongh@outlook.com>
2018-06-12 17:17:06 -0400

    add block list in network graph

--- src/app/layout/workspace/bhco/network-graph/network-graph.component.ts
+++ src/app/layout/workspace/bhco/network-graph/network-graph.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Statement} from "@angular/compiler";
+import {StateService} from "../../../../service/state.service";
+import {LocationService} from "../../../../service/location.service";
 
 @Component({
   selector: 'app-network-graph',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetworkGraphComponent implements OnInit {
 
-  constructor() { }
+  member = JSON.parse(localStorage.getItem('curMem'));
+  user = JSON.parse(localStorage.getItem('curUser'));
+  blocks: any[];
+
+  constructor(
+    private stateService: StateService,
+    private locationService: LocationService,
+  ) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.stateService.existMember$.next(true);
+    })
+    this.getBlockList();
+  }
+
+  getBlockList() {
+    this.locationService.getBlockByCommunity(this.user.location).subscribe(value => {
+      this.blocks = value;
+      console.log(this.blocks);
+    })
   }
 
 }
